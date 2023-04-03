f03d6ab0e5291162ccb8488475986959b7351b40
jason <jasonjas@gmail.com>
2023-04-03 06:11:01 -0500

    documentation

--- query_db.py
+++ query_db.py
@@ -22,7 +22,17 @@ from constants import DATABASE_FILE_NAME
 
 
 # type: (str, str, str, str) -> None
-def populate_database(database, json_index, src_file_path='index-full.json', dest_file_path='data.db'):
+def populate_database(table_name, json_index, src_file_path='index-full.json', dest_file_path='data.db'):
+    """Create database from pricing API index data
+
+    args:
+        table_name: name of table to create in the db file
+        json_index: top-level index of json to output, ex. 'products' or 'terms'
+        src_file_path: relative or full file path to source index json file
+        dest_file_path: relative or full file path to database that is wanted to be created/replaced
+
+    return: saves the output to a database file on the filesystem
+    """
     jsonfile = pd.read_json(src_file_path)
     products = jsonfile[json_index]
 
@@ -31,11 +41,20 @@ def populate_database(database, json_index, src_file_path='index-full.json', des
     prods.rename(columns=lambda s: s.lower().replace(
         'attributes.', 'attributes_'), inplace=True)
     con = sqlite3.connect(dest_file_path)
-    prods.to_sql(database, con)
+    prods.to_sql(table_name, con)
 
 
-def build_query(database: str, values: Dict[str, str], columns='*'):
-    db_string = f"SELECT {columns} FROM {database}"
+def build_query(table, values, columns='*'): # type: (str, Dict[str, str], str) -> str
+    """Create the query string used to query the database
+
+    args:
+        table: name of table to query in the database
+        values: dictionary of tables and search values to query the database
+        columns: the column(s) to return data for
+
+    return: the full query string
+    """
+    db_string = f"SELECT {columns} FROM {table}"
     where_clause_values = ''
     for col in values:
         if where_clause_values != '':
@@ -46,7 +65,14 @@ def build_query(database: str, values: Dict[str, str], columns='*'):
     return query_string
 
 
-def query_products(query_string: str):
+def query_products(query_string): # type: (str) -> any
+    """Query database using the query string provided
+
+    args:
+        query_string: database query string
+
+    return: results of the query
+    """
     cur = _create_connection()
     query_results = cur.execute(query_string)
     results = query_results.fetchall()
@@ -54,23 +80,43 @@ def query_products(query_string: str):
     return results
 
 
-def get_column_names(database: str):
+def get_column_names(table_name): # type: (str) -> list
+    """Retrieve the column names in the specified table
+
+    args:
+        table_name: name of the table to get columns from
+
+    return: list of column names
+    """
     cur = _create_connection()
-    cur.execute(f"select * from {database} limit 1")
+    cur.execute(f"select * from {table_name} limit 1")
     col_names = [i[0] for i in cur.description]
     cur.close()
     return col_names
 
 
-def _create_connection(db_file=DATABASE_FILE_NAME):
+def _create_connection(db_file=DATABASE_FILE_NAME): # type: (str) -> sqlite3.Cursor
+    """Create a sqlite connection to a database file
+
+    args:
+        db_file: relative or full file path to database
+
+    return: sqlite3 cursor object
+    """
     con = sqlite3.connect(db_file)
     cur = con.cursor()
     return cur
 
 
-def get_tables(db_file=DATABASE_FILE_NAME):
+def get_table_info(db_file=DATABASE_FILE_NAME, table_name='products'): # type (str, str) -> sqlite3.Cursor
+    """Get sqlite pragma table info in the database
+
+    args:
+        db_file: relative or full file path to database file
+
+    return: sqlite3 cursor object
+    """
     cur = _create_connection()
-    tables = cur.execute("PRAGMA table_info(products);")
-    print(tables.fetchall())
+    tables = cur.execute(f"PRAGMA table_info({table_name});")
     cur.close()
     return tables
